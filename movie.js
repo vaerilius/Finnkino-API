e3a447d228761cd217f60fd8407d4fe0b4ae8e7c
Vaerilius <tjohannest@gmail.com>
2019-03-04 07:50:53 +0200

    cleaning

--- movie.js
+++ movie.js
@@ -1,38 +1,32 @@
 class Movie {
   constructor() {
-    this.cityDefaultID = '1033';
-    this.day = new Date();
-   
-    this.daymodifed = `${this.day.getDate()}.${this.day.getMonth()}.${this.day.getFullYear()}`;
-    // console.log(this.daymodifed);
-   
+    this.cityDefaultID = "1033";
+    this.dayRaw = new Date();
+    this.day = `${this.dayRaw.getDate()}.${this.dayRaw.getMonth()}.${this.dayRaw.getFullYear()}`;
   }
 
   async getAreas() {
+    const areaResponse = await fetch(
+      `https://www.finnkino.fi/xml/TheatreAreas`
+    );
 
-    const cityResponse = await fetch(`https://www.finnkino.fi/xml/TheatreAreas`);
-
-    const scheduleSchedule = await cityResponse.text();
-
-    const areas = new DOMParser().parseFromString(scheduleSchedule, "text/xml");
-
-    return  areas;
+    const areas = await areaResponse.text();
+    return new DOMParser().parseFromString(areas, "text/xml");
   }
-
 
   async changeTheatreArea(city) {
     this.cityDefaultID = city;
   }
-  
+
   async getshows(cityId) {
     this.cityDefaultID = cityId;
-    const areaSchedule = await fetch(`https://www.finnkino.fi/xml/Schedule/?area=${this.cityDefaultID}&dt=${this.daymodifed}`);
-    const area = await areaSchedule.text();
-  
-    const schedule = new DOMParser().parseFromString(area, "text/xml");
-    return schedule;
+    const shows = await fetch(
+      `https://www.finnkino.fi/xml/Schedule/?area=${this.cityDefaultID}&dt=${
+        this.day
+      }`
+    );
+
+    const area = await shows.text();
+    return new DOMParser().parseFromString(area, "text/xml");
   }
-
-
-
 }
