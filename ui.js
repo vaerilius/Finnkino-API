a33ff98c6b3fe5cba67d2c363e665857e2235d74
Vaerilius <tjohannest@gmail.com>
2019-03-21 14:16:08 +0200

    update

--- ui.js
+++ ui.js
@@ -6,21 +6,22 @@ class UI {
     let output = `
       <h1 class="display-5 text-uppercase font-weight-bold text-center pt-3">Finnkino Movies</h1>
           <hr>
-          <div class="card-group">
+          <div class="card-group d-flex justify-content-around">
       `;
 
     shows.forEach(show => {
       output += `
-       <div class="col-ms-12 col-md-6 col-lg-4 col-xl-3 m-auto pb-4">
-        <div class="card" style="width: 15rem;">
-          <img class="card-img-top" src="${show.imgURL}" >
+       <div class="col-sm-6 col-md-6 col-lg-4 col-xl-3 px-md-3 bg-transparent ">
+        <div class="card mx-auto p-3 pb-4 bg-transparent border-0" style="width: 17rem;">
+        <div class="card-title mx-auto text-center bg-white border border-dark rounded p-2"><h4 class="display-5">${show.originalTitle}</h4>
+          <img class="card-img-top rounded" src="${show.imgURL}" >
           <div class="card-body">
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">${show.originalTitle}</li>
-              <li class="list-group-item">${show.theater}</li>
-              <li class="list-group-item">${show.start}</li>
-              <li class="list-group-item">${show.lengthInMinutes} Minutes</li>
+              <li class="list-group-item rounded"><h5 class="display-5">${show.theater}</h5></li>
+              <li class="list-group-item rounded">${show.start}</li>
+              <li class="list-group-item rounded">${show.lengthInMinutes} Minutes</li>
             </ul>
+            </div>
           </div>
          </div>
         </div>
